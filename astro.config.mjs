5768b0f8cd4d9ce27da9dfb19bfce477fd94b78f
April <95392008+apriltaoyvr@users.noreply.github.com>
2023-01-17 22:54:34 +0100

    Initialize

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), mdx(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })]
+});
---
+++ tailwind.config.cjs
@@ -0,0 +1,52 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      'light',
+      'black',
+      'retro',
+      'night',
+      {
+        rosepine: {
+          primary: '#c4a7e7',
+          secondary: '#ebbcba',
+          accent: '#f6c177',
+          neutral: '#191724',
+          'base-100': '#1f1d2e',
+          info: '#31748f',
+          success: '#9ccfd8',
+          warning: '#f6c177',
+          error: '#eb6f92',
+        },
+        'rosepine-moon': {
+          primary: '#c4a7e7',
+          secondary: '#ea9a97',
+          accent: '#c4a7e7',
+          neutral: '#2a273f',
+          'base-100': '#232136',
+          info: '#3e8fb0',
+          success: '#9ccfd8',
+          warning: '#f6c177',
+          error: '#eb6f92',
+        },
+        'rosepine-dawn': {
+          primary: '#907aa9',
+          secondary: '#d7827e',
+          accent: '#907aa9',
+          neutral: '#faf4ed',
+          'base-100': '#fffaf3',
+          info: '#286983',
+          success: '#56949f',
+          warning: '#ea9d34',
+          error: '#b4637a',
+        },
+      },
+    ],
+    darkTheme: 'night',
+  },
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+};
