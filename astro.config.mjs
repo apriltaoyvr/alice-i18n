f6b2ffbd629bbe984472b220c83f8065553946c1
April <95392008+apriltaoyvr@users.noreply.github.com>
2023-01-18 20:01:18 +0100

    Remove SSR; add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import image from '@astrojs/image';
-import vercel from '@astrojs/vercel/serverless';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    mdx(),
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp',
-    }),
-  ],
-  output: 'server',
-  adapter: vercel(),
+  integrations: [tailwind(), mdx(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), sitemap()],
+  site: 'https://alice-abridged.vercel.app/'
 });
