0906fb6e26bad757f2ea63fdae2e73bc6fbe6675
April <95392008+apriltaoyvr@users.noreply.github.com>
2023-01-19 20:17:27 +0100

    i18n complete for Italian and French

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import image from '@astrojs/image';
-import sitemap from "@astrojs/sitemap";
 import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx(), image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  }), sitemap(), astroI18next()],
-  site: 'https://alice-abridged.vercel.app/'
+  }), astroI18next()],
 });
