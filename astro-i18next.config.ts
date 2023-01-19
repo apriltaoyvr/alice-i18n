2f06fa2c20c29a8f2746b78897d71ae4ff88d3d3
April <95392008+apriltaoyvr@users.noreply.github.com>
2023-01-19 21:54:07 +0100

    Add German and Finnish to astro-i18next config

--- astro-i18next.config.ts
+++ astro-i18next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('astro-i18next').AstroI18nextConfig} */
 export default {
   defaultLocale: 'en',
-  locales: ['en', 'fr', 'it'],
+  locales: ['en', 'fr', 'it', 'de', 'fi'],
   namespaces: ['common'],
   defaultNamespace: 'common',
   i18nextServer: {
--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import image from '@astrojs/image';
-import astroI18next from "astro-i18next";
+import astroI18next from 'astro-i18next';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  }), astroI18next()],
+  integrations: [
+    tailwind(),
+    mdx(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+    astroI18next(),
+  ],
 });
